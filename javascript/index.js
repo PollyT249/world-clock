7bca9cb2278cb462219c7a5b34fce6dc50daf8c0
PollyT249 <polinatretiakova249@gmail.com>
2024-09-05 22:56:50 +0300

    Added single location current date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let tallinnElement = document.querySelector("#tallinn");
+  let tallinnDateElement = tallinnElement.querySelector(".date");
+  let tallinnTimeElement = tallinnElement.querySelector(".time");
+  let tallinnTime = moment().tz("Europe/Tallinn");
+  tallinnDateElement.innerHTML = tallinnTime.format("MMMM Do YYYY");
+  tallinnTimeElement.innerHTML = tallinnTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
 
+  let stockholmElement = document.querySelector("#stockholm");
+  let stockholmDateElement = stockholmElement.querySelector(".date");
+  let stockholmTimeElement = stockholmElement.querySelector(".time");
+  stockholmTime = moment().tz("Europe/Stockholm");
+  stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
+  stockholmTimeElement.innerHTML = stockholmTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
